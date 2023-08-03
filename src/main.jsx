75383ea03695a8c9b0856d31a6fa3f85c788517f
Louis-Philippe <lpppicard@attestra.com>
2023-08-02 21:08:29 -0400

    init

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.fluttermapp.com}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
