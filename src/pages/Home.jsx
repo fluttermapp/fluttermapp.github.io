9a114142f853f5890584496c4490f6aa6ee5d112
Louis-Philippe <info@fluttermapp.com>
2023-09-25 16:11:13 -0400

    One course only

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ export default function Home() {
                     <Stats />
                     <Why />
                     <ZeroToHero />
-                    <CleanArchitecture />
+                    {/*<CleanArchitecture />*/}
                     <Testimonials />
                     {/*<Clients />*/}
                     <CTA />
