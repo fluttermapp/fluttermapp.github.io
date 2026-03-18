ac862277b8e87b5e6453b1179e415baa0c5752bb
Louis-Philippe <info@fluttermapp.com>
2026-03-17 22:51:41 -0400

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Link as ScrollLink } from "react-scroll";
 
 import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
@@ -12,24 +11,14 @@ const Navbar = () => {
       <img src={logo} alt="flutter mapp" className="w-[50px] h-[50px]" />
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
-        {navLinks.map((nav, index) => (
+        {navLinks.map((nav, index) =>
           <li
             key={nav.id}
-            className={`font-poppins cursor-pointer text-[16px] ${active === nav.title
-              ? "text-black font-black"
-              : "text-dimBlack font-normal"
-              } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
-            onClick={() => setActive(nav.title)}
-          >
-            <ScrollLink
-              to={nav.id}      // matches the id of the section
-              smooth={true}    // smooth scrolling
-              duration={500}   // animation duration in ms
-            >
-              {nav.title}
-            </ScrollLink>
+            className={`font-poppins cursor-pointer text-[16px] ${active === nav.title ? "text-black font-black" : "text-dimBlack font-normal"} ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+            onClick={() => setActive(nav.title)}>
+            <a href={`#${nav.id}`}>{nav.title}</a>
           </li>
-        ))}
+        )}
       </ul>
     </nav>
   );
